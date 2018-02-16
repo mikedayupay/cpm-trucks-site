a78608d87fbbe5c65295e33f6b12a536dcf23630
mikedayupay <jandayupay@gmail.com>
2018-02-16 16:47:07 +0800

    new routing rules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 import { TrucksComponent} from './trucks/trucks.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { TruckDetailsComponent } from './truck-details/truck-details.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent }, 
-  { path: 'trucks', component: TrucksComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about-us', component: AboutUsComponent }, 
+  { path: 'trucks/:type', component: TrucksComponent },
+  { path: 'truck-detail/:id', component: TruckDetailsComponent },
   { path: 'contact-us', component: ContactUsComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: HomeComponent }
 ]
 
 @NgModule({
