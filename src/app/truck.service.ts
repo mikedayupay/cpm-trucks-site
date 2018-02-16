1c36f5a8f19b01ca2197487cb3cdac6438faf150
mikedayupay <jandayupay@gmail.com>
2018-02-16 16:47:31 +0800

    truck service observable firebase

--- src/app/truck.service.ts
+++ src/app/truck.service.ts
@@ -1,13 +1,31 @@
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { of } from 'rxjs/Observable/of';
+import { of } from 'rxjs/observable/of';
 
 import { Truck } from './truck';
+import { AngularFireList, AngularFireObject } from 'angularfire2/database/interfaces';
 
 @Injectable()
 export class TruckService {
 
-  constructor() { }
+  private basePath: string = 'trucks';
+  
+  trucks = null;
+  truck = null;
+
+  
+  constructor(private db: AngularFireDatabase) { }
+  
+  getTrucks(type: string): Observable<Truck[]>{
+    
+  this.trucks = this.db.list(this.basePath, ref => ref.orderByChild('type').equalTo(type)).valueChanges();
+  return this.trucks;
+  }
+
+  getTruck(id: number): Observable<Truck> {
+    this.truck = this.db.list(this.basePath, ref => ref.orderByChild('id').equalTo(id)).valueChanges();
+    return this.truck;
+  }
 
 }
