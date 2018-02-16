09a089e98bb1dd485e7f5d1de7f2b70a8276c444
mikedayupay <jandayupay@gmail.com>
2018-02-16 16:45:29 +0800

    truck component fixed

--- src/app/trucks/trucks.component.ts
+++ src/app/trucks/trucks.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
+import { ActivatedRoute } from '@angular/router';
+import { TruckService } from '../truck.service';
+
+import { Truck } from '../truck';
 
 
 @Component({
@@ -10,20 +13,23 @@ import { Observable } from 'rxjs/Observable';
 })
 export class TrucksComponent implements OnInit {
 
+  trucksObservable: Observable<Truck[]>;
+  title: string;
 
-  trucksObservable: Observable<any[]>;
-
-  constructor(private db: AngularFireDatabase) { }
-
-  ngOnInit() {
-    this.trucksObservable = this.getTrucks('/trucks');
-    
+  constructor(private route: ActivatedRoute, private truckService: TruckService) 
+  {
+    this.route.params.subscribe(params => {
+      console.log(params)
+      if (params['type']) {
+        this.getTrucks(params['type']);
+        this.title = params['type'] + "-duty models";
+      }
+    });
   }
 
-  getTrucks(listPath) : Observable<any[]> {
-    return this.db.list(listPath).valueChanges();
+  ngOnInit() {}
+
+  getTrucks(type) : void {
+    this.trucksObservable =  this.truckService.getTrucks(type);
   }
-
-  
-
 }
